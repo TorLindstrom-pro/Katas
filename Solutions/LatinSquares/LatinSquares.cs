2365bdd880734f8958e7ec643f0b8dae2e99c395
TorLindstrom <tor.lindstrom@winlas.se>
2024-10-04 14:00:05 +0200

    kata: Latin squares

---
+++ Solutions/LatinSquares/LatinSquares.cs
@@ -0,0 +1,24 @@
+﻿namespace LatinSquares;
+
+public class LatinSquare
+{
+	public static int[][] MakeLatinSquare(int n)
+	{
+		if (n < 1)
+		{
+			throw new ArgumentException("n must be at least 1");
+		}
+		
+		int[][] latinSquare = new int[n][];
+		for (int i = 0; i < n; i++)
+		{
+			latinSquare[i] = new int[n];
+			for (int j = 0; j < n; j++)
+			{
+				latinSquare[i][j] = (j + 1 + i) % n + 1;
+			}
+		}
+
+		return latinSquare;
+	}
+}
---
+++ Solutions/LatinSquares/LatinSquaresTests.cs
@@ -0,0 +1,33 @@
+﻿namespace LatinSquares;
+
+public class LatinSquaresTests
+{
+	[Test]
+	public void MakeLatinSquare_RowShouldNotContainDuplicates()
+	{
+		int[][] square = LatinSquare.MakeLatinSquare(4);
+		Assert.That(square[0], Is.Unique);
+	}
+	
+	[Test]
+	public void MakeLatinSquare_ColumnsShouldNotContainDuplicates()
+	{
+		int[][] square = LatinSquare.MakeLatinSquare(4);
+		var column = square.Select(r => r[0]);
+		Assert.That(column, Is.Unique);
+	}
+	
+	[Test]
+	public void MakeLatinSquare_AllNumbersAreWithinTheGivenRange()
+	{
+		int[][] square = LatinSquare.MakeLatinSquare(4);
+		int[] flattenedSquare = square.SelectMany(innerArray => innerArray).ToArray();
+		Assert.That(flattenedSquare, Is.All.InRange(1,4));
+	}
+	
+	[Theory]
+	public void MakeLatinSquare_NonPositiveSize_ThrowsException([Range(-1,0)] int size)
+	{
+		Assert.Throws<ArgumentException>(() => LatinSquare.MakeLatinSquare(size));
+	}
+}
