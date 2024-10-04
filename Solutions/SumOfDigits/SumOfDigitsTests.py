a0f1ea25b16331690f767d88d38b083b8c52be6e
TorLindstrom <tor.lindstrom@winlas.se>
2024-10-04 16:53:03 +0200

    kata: sum of digits
    first try, apparently you can do a lot more in python than I know about, of course! Needs refactoring

---
+++ Solutions/SumOfDigits/SumOfDigitsTests.py
@@ -0,0 +1,23 @@
+﻿import unittest
+
+class SumOfDigits:
+    @staticmethod
+    def digital_root(n: int) -> int:
+        string = str(n)
+        while len(string) > 1:
+            string = str(SumOfDigits.sum_of_digits(string))
+        return int(string)
+
+    @staticmethod
+    def sum_of_digits(string) -> int:
+        result = 0
+        for char in string:
+            result += int(char)
+        return result
+
+
+class MoveTenTests(unittest.TestCase):
+
+    def test_digitalRoot_FindsRoot(self):
+        self.assertEqual(SumOfDigits.digital_root(12345), 6)
+    
