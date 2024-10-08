4320ee3cafea9a67e47246b3803b65db642fd497
TorLindstrom <tor.lindstrom@winlas.se>
2024-10-08 14:35:09 +0200

    kata: Atm
    a rework from a while solution

---
+++ Solutions/ATM/Atm.cs
@@ -0,0 +1,18 @@
+﻿namespace Solutions.ATM;
+
+public class Atm
+{
+	public static int solve(int n) {
+		if (n < 10 || n % 10 > 0) {
+			return -1;
+		}
+      
+		List<int> notes = [500, 200, 100, 50, 20, 10];
+		
+		return notes.Aggregate(new Tuple<int, int>(n, 0), (accumulated, note) =>
+		{
+			var (quotient, remainder) = int.DivRem(accumulated.Item1, note);
+			return new Tuple<int, int>(remainder, accumulated.Item2 + quotient);
+		}).Item2;
+	} 
+}
---
+++ Solutions/ATM/AtmTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace Solutions.ATM;
+
+public class AtmTests
+{
+	[TestCase(1, ExpectedResult = -1)]
+	[TestCase(0, ExpectedResult = -1)]
+	[TestCase(5, ExpectedResult = -1)]
+	public int Solve_LessThan10_ReturnsNegativeOne(int amount)
+	{
+		return Atm.solve(amount);
+	}
+	
+	[TestCase(10, ExpectedResult = 1)]
+	[TestCase(20, ExpectedResult = 1)]
+	[TestCase(500, ExpectedResult = 1)]
+	public int Solve_MathingANote_ReturnsOne(int amount)
+	{
+		return Atm.solve(amount);
+	}
+	
+	[TestCase(30, ExpectedResult = 2)]
+	[TestCase(260, ExpectedResult = 3)]
+	[TestCase(1620, ExpectedResult = 5)]
+	public int Solve_AmountIsCombinationOfNotes_ReturnsMinimumNumberOfNotes(int amount)
+	{
+		return Atm.solve(amount);
+	}
+}
